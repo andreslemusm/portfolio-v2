e263f0222fbc3fc8551b902034d79a93593fdf5a
Andres Lemus <aclmadrid04@gmail.com>
2021-08-07 14:10:09 -0500

    feat: add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,27 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" type="image/svg" href="/favicon.svg" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#101010" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body className="bg-gray-900 font-sans text-white antialiased">
           <Main />
